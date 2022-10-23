3d917487579c024e057b3a044a5fb638b7714cce
benjamin <bnymynmqwnn@gmail.com>
2022-10-24 00:56:55 +0300

    clen

--- benjamin-protfolio/components/Nav.js
+++ benjamin-protfolio/components/Nav.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import { BsFillMoonStarsFill } from "react-icons/bs";
-import Link from 'next/link';
-const Nav = ({activeDarkMode}) => {
+import { BsFillMoonStarsFill,BsSun } from "react-icons/bs";
+const Nav = ({activeDarkMode,darkMode}) => {
+
   return (
     <nav className=" py-10 mb-12 flex justify-between bg-white dark:bg-gray-900 dark:text-white sticky top-0 z-10">
             <h1 className="font-medium text-xl">Benjamin Makonen Portfolio</h1>
             <ul className="flex items-center">
               <li>
+                {darkMode?
                 <BsFillMoonStarsFill
-                  onClick={activeDarkMode}
-                  className=" cursor-pointer text-2xl"
+                onClick={activeDarkMode}
+                className=" cursor-pointer text-2xl"
                 />
+              :
+              <BsSun
+              onClick={activeDarkMode}
+              className=" cursor-pointer text-2xl"
+              />
+              }
               </li>
               <li
-              
                 className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
               >
-                <Link
-                  href="resume"
-                >
+              
+                <a href='https://content.celero.io/s/56798897/binyamin-cv/?is_fit_page_zoom=true' target='blank'>
+
                   Resume
-                </Link>
+                </a>
               </li>
             </ul>
           </nav>
--- benjamin-protfolio/pages/index.js
+++ benjamin-protfolio/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   const activeDarkMode = () =>{
    setDarkMode(!darkMode)
   }
-  // const skills = [css3, github, html5, js, mysql, node, sass];
+
   return (
     <div className={darkMode ? "dark" : ""}>
       <Head>
@@ -24,90 +24,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-        {/* <section className="min-h-[90vh]"> */}
-          <Nav activeDarkMode={activeDarkMode}/>
+          <Nav activeDarkMode={activeDarkMode} darkMode={darkMode}/>
           <Main me={me} />
-        {/* </section> */}
-        {/* <section> */}
           <About me={me} />
           <Skills />
-        {/* </section> */}
-        {/* <section className="py-10"> */}
-          {/* <div>
-            <Link href='/test'>
-            <h3 className="text-3xl py-1 dark:text-white ">My Project</h3>
-            </Link>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div> */}
           <Projects/>
-          {/* <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg "
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={youTube}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className=""
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={spotifyApp}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
-            </div>
-          </div> */}
-        {/* </section> */}
       </main>
     </div>
   );
