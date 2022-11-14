ccff8aa7d14c6c52d57444aa6e394b2c608d013b
benjamin <bnymynmqwnn@gmail.com>
2022-11-14 17:06:45 +0200

    about

--- benjamin-protfolio/components/About.jsx
+++ benjamin-protfolio/components/About.jsx
@@ -13,17 +13,10 @@ const About = ({me}) => {
           </p>
           <h2 className='py-4 dark:text-white'>Who I Am</h2>
           <p className='py-2 text-gray-600 dark:text-white'>
-          Hi, I am a graduate of the software development course at Tech-Career College. From a young age I have always been deeply fascinated by computers and technology. After many thoughts I decided that software development is the profession I would like to pursue. Along with the skills I acquired in college and my personal skills, I will bring with me a strong work ethic, self-learning ability, high motivation to learn new systems and good teamwork.
+          I am a graduate of the software development course at Tech-Career College. From a young age I have always been deeply fascinated by computers and technology. After giving it much thought, I decided that software development is the profession I will pursue. In addition to the skills I acquired in college, I will bring a strong work ethic, self-learning ability, high motivation to learn new systems and good teamwork.
           </p>
           <p className='py-2  text-gray-600 dark:text-white'>
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            I specialize in building mobile responsive front-end UI applications that connect with API’s and other backend technologies. I’m passionate about learning new technologies and understand there is more than one way to accomplish a task. Though I am most proficient in building front-end applications using HTML, CSS, JavaScript, and React, I am a quick learner and can pick up new tech stacks as needed. I believe that being a great developer is not using one specific language but choosing the best tool for the job.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
